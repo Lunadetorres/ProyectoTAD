b0e9a40301e2d4e91ff7e6a7e7377784d0611b68
jsenfra123 <58392000+jsenfra123@users.noreply.github.com>
2023-04-29 13:20:33 +0200

    Cambios diseño

--- ProyectoFinal/resources/views/checkout.blade.php
+++ ProyectoFinal/resources/views/checkout.blade.php
@@ -5,53 +5,51 @@
     <h1>Checkout</h1>
     <form method="POST" action="{{ route('checkout.process') }}">
         @csrf
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="name">Nombre</label>
             <input type="text" class="form-control" id="name" name="name" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="email">Email</label>
             <input type="email" class="form-control" id="email" name="email" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="address">Dirección</label>
             <input type="text" class="form-control" id="address" name="address" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="city">Ciudad</label>
             <input type="text" class="form-control" id="city" name="city" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="state">País</label>
             <input type="text" class="form-control" id="state" name="state" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="zip">Código Postal</label>
             <input type="text" class="form-control" id="zip" name="zip" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="card-number">Número de Tarjeta</label>
             <input type="text" class="form-control" id="card-number" name="card-number" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="card-expiry">Fecha expiración</label>
             <input type="text" class="form-control" id="card-expiry" name="card-expiry" placeholder="MM/YY" required>
         </div>
-
-        <div class="form-group">
+        <div class="form-group col-lg-4 mb-3">
             <label for="card-cvc">CVC</label>
             <input type="text" class="form-control" id="card-cvc" name="card-cvc" required>
         </div>
-
-        <button type="submit" class="bg-white text-dark btn btn-primary">Realizar pago</button>
+        <button type="submit" class="bg-primary text-white btn btn-primary ">
+            <div class="d-flex justify-content-center">
+                <p class="h5"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16">
+                    <path fill="#50D51A" d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
+                    <path fill="#50D51A" d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z"/>
+                </svg>
+                Pagar</p>    
+            </div>
+        </button>
     </form>
 </div>
 @endsection
--- ProyectoFinal/resources/views/productos.blade.php
+++ ProyectoFinal/resources/views/productos.blade.php
@@ -3,24 +3,24 @@
 @section('content')
 
 <div class="container">
-    <h1>Productos</h1>
-    <div class="row">
+    <h1 class="text-center mb-3" style="color:#88389c;">TU COSMÉTICA ONLINE</h1>
+    <div class="row ">
         @foreach ($productos as $producto)
-        <div class="col-md-4">
-            <div class="card mb-4">
-                <img src="{{ $producto->imagenUrl }}" class="img-fluid mb-3" alt="{{ $producto->nombre }}">
-                <div class="card-body">
-                    <h5 class="card-title">Producto: {{ $producto->nombre }}</h5>
-                    <p class="card-text">Descripción: {{ $producto->descripcion }}</p>
-                    <p class="card-text">Precio: {{ $producto->precio }}€</p>
+        <div class="col-sm-3 mb-5 border-0" >
+            <div class="card border-0" style=" width:200px; " >
+                <img src="/images/{{ $producto->imagenUrl }}"  style="height: 200px; width:200px;" class="img-fluid mb-1" alt="{{ $producto->nombre }}">
+                <div class="card-body text-center m-0 p-0" style="max-height: 150px;">
+                    <h5 class="card-title m-0 p-0">{{ $producto->nombre }}</h5>
+                    <p class="card-text m-0 p-0">{{ $producto->descripcion }}</p>
+                    <p class="card-text h5 m-0 mb-1 p-0" style="color:#88389c;"> {{ $producto->precio }}€</p>
                     <!-- <a href="#" class="btn btn-primary">Add cart</a> -->
                     <form action="{{ route('cart.add', $producto) }}" method="POST">
                         @csrf
-                        <button type="button" class="btn btn-secondary ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
-            </svg>
-                          </button>
+                        <button type="button " class="btn btn-lila mb-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path fill="#ffffff" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
+                            </svg>
+                        </button>
                     </form>
                 </div>
             </div>
